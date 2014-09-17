70a67687eb97c6c9f837827ae083105824780d8b
clobito <clobo@misena.edu.co>
2014-09-17 16:36:00 -0500

    Proyecto Gestor de Calzado Propósito: Intentar cargar el control datepicker() de Bootstrap en una vista. Fuentes afectados: 1.VISTA 1.1.base.html 1.2.crear_compra.html 2.Biblioteca JAVASCRIPT 2.1.biblioteca.js

---
+++ gestorApp/static/js/biblioteca.js
@@ -0,0 +1,14 @@
+/* Proyecto Software Gestión de Manufactura
+Biblioteca de funciones 
+Fecha creado: 17/09/2014
+*/
+
+$(document).ready(function()
+{
+	$('#id_Fecha').datepicker();
+});
+
+$('#test_btn').click(function()
+{
+	alert("Ingresa");
+});
