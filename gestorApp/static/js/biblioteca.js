b4de8a6c89d382dfbf4b9e4165782c2eaecbd8fb
clobito <clobo@misena.edu.co>
2014-09-18 16:50:33 -0500

    Proyecto Gestor de Calzado Propósito: Cargue del control datepicker a la vista de geenración de compra. Fuentes afectados 1.Javascript 1.1.biblioteca.js, método ready() 1.2.jquery.ui.datepicker.js 1.3.jquery-ui.js 2.Vistas 2.1.crear_compra.html 3.Vista (Frontend) 3.1.base.html 3.2.jquery-ui.css 3.3.jquery.ui.datepicker.css

--- gestorApp/static/js/biblioteca.js
+++ gestorApp/static/js/biblioteca.js
@@ -1,14 +1,67 @@
 /* Proyecto Software Gestión de Manufactura
 Biblioteca de funciones 
 Fecha creado: 17/09/2014
+Fecha actualizado: 18/09/2014
+Cambio realizado: Configuración del componente fecha empleando datepicker como plugin de jquery.
+Observaciones: Componente Datepicker en español en http://www.bufa.es/jquery-ui-datepicker-espanol/
 */
 
 $(document).ready(function()
 {
-	$('#id_Fecha').datepicker();
+	$('#id_Fecha').datepicker({		
+		language: 'es',
+		closeText: 'Cerrar',
+		defaultDate: '+0',
+		maxDate: '+8d',
+		minDate: '0d',		
+		monthNames: ["Enero",
+		"Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+		monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+		dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mie','Jue','Vie','Sab'],
+		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sa'],
+		showWeek: 'True',
+		dateFormat: 'yy-mm-dd',
+		firstDay: 1,
+	});
 });
+/* Métodos del template base.html 
+Fecha actualizado: 18/09/2014
+*/
 
-$('#test_btn').click(function()
-{
-	alert("Ingresa");
-});
+function activar(capa){
+	//var u;
+	var capa_actual=capa;
+	//$("#menu").hide();
+	//oculta el menu
+	document.getElementById("menu").className="navbar-collapse collapse";//para interfaz movil, retrae menu
+	desactivar();
+	if(document.getElementById(capa_actual).className=="container hidden"){
+		document.getElementById(capa_actual).className="container";
+		document.getElementById(capa_actual).disabled=false;
+		
+		}
+		
+	else if(document.getElementById(capa_actual).className=="container"){
+		document.getElementById(capa_actual).className="container hidden";
+		document.getElementById(capa_actual).disabled=true;
+		
+		}	
+		
+}
+
+function desactivar(){
+	//desactiva las capas visuales del menu principal
+		document.getElementById("admin_principal").className="container hidden";
+		document.getElementById("admin_principal").disabled=true;
+		document.getElementById("gestionInv_principal").className="container hidden";
+		document.getElementById("gestionInv_principal").disabled=true;
+		document.getElementById("gestionFact_principal").className="container hidden";
+		document.getElementById("gestionFact_principal").disabled=true;
+		document.getElementById("gestionCostos_principal").className="container hidden";
+		document.getElementById("gestionCostos_principal").disabled=true;
+		document.getElementById("gestionProdProg_principal").className="container hidden";
+		document.getElementById("gestionProdProg_principal").disabled=true;
+		document.getElementById("gestionProdFlujos_principal").className="container hidden";
+		document.getElementById("gestionProdFlujos_principal").disabled=true;		
+	}
